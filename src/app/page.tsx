3df3b25fab348809b667e38a787416f9bfc0a6e7
THERAMREDDY DASAVANTH REDDY <71059812+TheramReddy@users.noreply.github.com>
2024-09-27 14:25:22 -0400

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,13 +16,13 @@ const perks = [
     name: 'Instant Delivery',
     Icon: ArrowDownToLine,
     description:
-      'Get your assets delivered to your email in seconds and download them right away.',
+      'Get your products delivered as soon as in 1 day',
   },
   {
     name: 'Guaranteed Quality',
     Icon: CheckCircle,
     description:
-      'Every asset on our platform is verified by our team to ensure our highest quality standards. Not happy? We offer a 30-day refund guarantee.',
+      'Every product on our platform is verified by our team to ensure our highest quality standards. Not happy? We offer a 30-day refund guarantee.',
   },
   {
     name: 'For the Planet',
